54f141878a13853a2e4e72bd63b5a0b54e5f040a
Karel Kivimägi <karel.kivimagi@khk.ee>
2020-01-27 13:30:02 +0200

    Raamistiku kasutus

---
+++ app/controllers/Pages.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Pages extends Controller
+{
+  /**
+   * Page constructor.
+   */
+  public function __construct()
+  {
+    $this->pagesModel = $this->model('Page');
+  }
+
+  public function test(){
+    $usersCount = $this->pagesModel->usersCount();
+    echo '<pre>';
+    print_r($usersCount);
+    echo '</pre>';
+  }
+
+  public function edit(){
+    $this->view('pages/edit');
+  }
+
+  public function index() {
+    $data = array('title' => 'Welcome');
+    $this->view('pages/index', $data);
+  }
+}
