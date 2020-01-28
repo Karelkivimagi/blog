ba2db290fa0c1a67584c3992555b3fd3a0350332
Karel Kivimägi <karel.kivimagi@khk.ee>
2020-01-28 09:16:50 +0200

    Registreerimine ja logimine, andmebaasi loomine

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,13 +1,13 @@
 <?php
-// projekti nimi
-define('PROJECT_NAME', 'pvk');
-// juur url
+// Project Name
+define('PROJECT_NAME', 'blog');
+// App Root Directory
+define('APPROOT', dirname(dirname(__FILE__)) );
+// URL Root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
-// rakenduse juurkataloog
-define('APPROOT', dirname(dirname(__FILE__)));
 
-// db parameetrid
+// database config
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'pdo');
+define('DB_NAME', PROJECT_NAME);
